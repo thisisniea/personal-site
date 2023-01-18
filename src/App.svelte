66fcfedb944d0d5362ce6c58bf1798801d3df958
Brandon <hello@niea.me>
2023-01-18 16:03:55 -0500

    feat: fix top site padding

--- src/App.svelte
+++ src/App.svelte
@@ -25,21 +25,8 @@
 
 <style lang="postcss">
   main {
-    padding: 4rem 1rem 1rem 1rem;
+    padding: 5rem 1rem 1rem 1rem;
     max-width: 800px;
     margin: 0 auto;
   }
-
-  @media (min-width: 600px) {
-    main {
-      padding: 6rem 1rem 1rem 1rem;
-    }
-  }
-
-  @media (min-width: 800px) {
-    main {
-      padding: 8rem 2rem 1rem 2rem;
-    }
-  }
-
 </style>
