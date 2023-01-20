cff3ff38c766e5d8edce364fe788a2178f59c0d5
Brandon <hello@niea.me>
2023-01-20 16:52:35 -0500

    chore: create stub pages for projects and tools

--- src/pages/Projects.svelte
+++ src/pages/Projects.svelte
@@ -1,1 +1,13 @@
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+    <title>Projects | niea.me</title>
+</svelte:head>
+
 <h1>Projects</h1>
+
+<style lang="postcss">
+    
+</style>
--- src/pages/Tools.svelte
+++ src/pages/Tools.svelte
@@ -1,1 +1,13 @@
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+    <title>Tools | niea.me</title>
+</svelte:head>
+
 <h1>Tools</h1>
+
+<style lang="postcss">
+    
+</style>
